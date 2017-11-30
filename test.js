feebfc2e904c95eadae2ab50f30f3eac9e79098a
indmind <sattanationmail@gmail.com>
2017-12-01 04:45:32 +0700

    edit password to sugarlabs

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const hello = require("./");
 // const hello = require("codein-hello-world-task");
 
-hello("dogci").then(mess => log(mess)).catch(mess => log(mess));
+hello("sugarlabs").then(mess => log(mess)).catch(mess => log(mess));
 
 const log = mess => console.log(mess);
